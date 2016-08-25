5ea360f0771d8269efe830c7fa62013a03311928
easyhaipi <easy_qingsong@163.com>
2016-08-25 17:30:33 +0800

    引导页面和广告页面的导入

--- AdvertiseDemo/AppDelegate.m
+++ AdvertiseDemo/AppDelegate.m
@@ -8,6 +8,10 @@
 
 #import "AppDelegate.h"
 
+
+
+#import "introductoryPagesHelper.h"
+#import "AdvertiseHelper.h"
 @interface AppDelegate ()
 
 @end
@@ -17,8 +21,60 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    
+  
+ 
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    
+    
+    [self setupIntroductoryPage];
+    
+    [self setupAdveriseView];
     return YES;
 }
+
+#pragma mark 引导页
+-(void)setupIntroductoryPage
+{
+    
+    NSString * oldVerson = [NSString stringWithFormat:@"%@",[[NSUserDefaults standardUserDefaults] stringForKey:@"firstVersion"]];
+    NSString * nowVerson = [NSString stringWithFormat:@"%@",VERSION];
+    
+    if(![[NSUserDefaults standardUserDefaults] stringForKey:@"firstVersion"] || [oldVerson isEqualToString:nowVerson] == NO)
+    {
+        
+        NSLog(@"第一次启动");
+        NSArray *images=@[@"introductoryPage1",@"introductoryPage2",@"introductoryPage3",@"introductoryPage4"];
+        [introductoryPagesHelper showIntroductoryPageView:images];
+        
+    } else
+    {
+        return;
+        
+    }
+
+    
+  
+}
+
+
+#pragma mark 启动广告
+-(void)setupAdveriseView
+{
+    // TODO 请求广告接口 获取广告图片
+    
+    //现在了一些固定的图片url代替
+    NSArray *imageArray = @[@"http://imgsrc.baidu.com/forum/pic/item/9213b07eca80653846dc8fab97dda144ad348257.jpg", @"http://pic.paopaoche.net/up/2012-2/20122220201612322865.png", @"http://img5.pcpop.com/ArticleImages/picshow/0x0/20110801/2011080114495843125.jpg", @"http://www.mangowed.com/uploads/allimg/130410/1-130410215449417.jpg"];
+    
+    [AdvertiseHelper showAdvertiserView:imageArray];
+}
+
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- AdvertiseDemo/ViewController.m
+++ AdvertiseDemo/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(pushToAd) name:NotificationContants_Advertise_Key object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +27,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+//进入广告页面
+-(void)pushToAd
+{
+    NSLog(@"进入广告页面");
+}
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+}
 @end
